d8f2ff586800963d41b55b28ee07b3a178ddfd4e
Daniel Pedersoli <dpmsengineer@gmail.com>
2025-01-05 18:24:20 -0300

    que isso Jesus

--- build/server.js
+++ build/server.js
@@ -159,7 +159,8 @@ app.register(transactionsRoutes, {
 
 // src/server.ts
 app.listen({
-  port: env.PORT
+  port: env.PORT,
+  host: "RENDER" in process.env ? `0.0.0.0` : `localhost`
 }).then(() => {
   console.log("HTTP Server Running!");
 });
